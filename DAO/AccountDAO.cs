e022948e23091e8ae36e87cbb3ee0dab8c90073f
Ho Xuan Hieu <hxhieu123@gmail.com>
2022-11-11 18:42:13 +0700

    Add project files.

---
+++ DAO/AccountDAO.cs
@@ -0,0 +1,99 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class AccountDAO
+    {
+        private static AccountDAO instance;
+
+        public static AccountDAO Instance
+        {
+            get { if (instance == null) instance = new AccountDAO(); return instance; }
+            private set { instance = value; }
+        }
+
+        private AccountDAO() {}
+      
+
+        //Login with userName and password
+        public bool Login(string userName, string passWord)
+        {
+            Account account = GetAccountByUserName(userName);
+            if (account == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (account.Password == passWord)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        
+        
+        public bool UpdateAccount(string userName, string displayName, string pass, string newPass)
+        {
+            int result = DataAccess.Instance.ExecuteNonQuery("exec USP_UpdateAccount @userName , @displayName , @password , @newPassword", new object[] { userName, displayName, pass, newPass });
+
+            return result > 0;
+        }
+
+        public DataTable GetListAccount()
+        {
+            return DataAccess.Instance.ExecuteQuery("SELECT * FROM dbo.Account");
+        }
+
+        public Account GetAccountByUserName(string userName)
+        {
+            DataTable data = DataAccess.Instance.ExecuteQuery("Select * from account where userName = '" + userName + "'");
+
+            foreach (DataRow item in data.Rows)
+            {
+                return new Account(item);
+            }
+
+            return null;
+        }
+
+        public bool InsertAccount(string name, string displayName, int type, string pass)
+        {
+            string query = string.Format("INSERT dbo.Account ( UserName, DisplayName, Type, password )VALUES  ( N'{0}', N'{1}', {2}, N'{3}')", name, displayName, type, pass);
+            int result = DataAccess.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool UpdateAccount(string name, string displayName, int type, string pass)
+        {
+            string query = string.Format("UPDATE dbo.Account SET DisplayName = N'{1}', Type = {2} WHERE UserName = N'{0}'", name, displayName, type, pass);
+            int result = DataAccess.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool DeleteAccount(string name)
+        {
+            string query = string.Format("Delete Account where UserName = N'{0}'", name);
+            int result = DataAccess.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool ResetPassword(string name, string pass)
+        {
+            string query = string.Format("update account set password = N'{0}' where UserName = N'{1}'",pass, name);
+            int result = DataAccess.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+    }
+}
---
+++ DAO/TableDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+
+namespace DAO
+{
+    public class TableDAO
+    {
+        private static TableDAO instance;
+
+        public static TableDAO Instance
+        {
+            get { if (instance == null) instance = new TableDAO(); return TableDAO.instance; }
+            private set { TableDAO.instance = value; }
+        }
+
+        public static int TableWidth = 90;
+        public static int TableHeight = 90;
+
+        private TableDAO() { }
+        public void setStatus(int id)
+        {
+            DataAccess.Instance.ExecuteNonQuery("UPDATE dbo.TableFood SET status = N'Có Khách' WHERE id = " + id);
+        }
+        public void setEmpty(int id)
+        {
+            DataAccess.Instance.ExecuteNonQuery("UPDATE dbo.TableFood SET status = N'Trống' WHERE id = " + id);
+        }
+        public void SwitchTable(int id1, int id2)
+        {
+            DataAccess.Instance.ExecuteQuery("USP_SwitchTabel @idTable1 , @idTabel2", new object[]{id1, id2});
+        }
+
+        public List<Table> LoadTableList()
+        {
+            List<Table> tableList = new List<Table>();
+
+            DataTable data = DataAccess.Instance.ExecuteQuery("USP_GetTableList");
+
+            foreach (DataRow item in data.Rows)
+            {
+                Table table = new Table(item);
+                tableList.Add(table);
+            }
+
+            return tableList;
+        }
+    }
+}
